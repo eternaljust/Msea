ac43de8f3377d386b1b919850a11ad51d3456d48
tzqiang <tzqiang1118@163.com>
2021-12-03 14:18:37 +0800

    Initial Commit

---
+++ Msea/MseaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MseaApp.swift
+//  Msea
+//
+//  Created by tzqiang on 2021/12/3.
+//
+
+import SwiftUI
+
+@main
+struct MseaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
