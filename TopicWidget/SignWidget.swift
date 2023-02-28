da643105df82a51365b73d376a923f1749485f21
eternaljust <tzqiang1118@163.com>
2023-02-28 11:02:00 +0800

    新增锁屏小组件：签到文本

--- TopicWidget/SignWidget.swift
+++ TopicWidget/SignWidget.swift
@@ -8,28 +8,64 @@
 
 import SwiftUI
 import WidgetKit
+import Kanna
 
 struct SignProvider: TimelineProvider {
+    private static let widgetGroup: UserDefaults? = UserDefaults(suiteName: "group.com.eternaljust.Msea.Topic.Widget")
+
     typealias Entry = SignEntry
 
     func placeholder(in context: Context) -> SignEntry {
-        SignEntry(date: .now)
+        SignEntry(date: .now, sign: SignModel())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SignEntry) -> Void) {
-        let entry = SignEntry(date: .now)
+        let entry = SignEntry(date: .now, sign: SignModel())
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<SignEntry>) -> Void) {
-        let entry = SignEntry(date: .now)
-        let timeline = Timeline(entries: [entry], policy: .atEnd)
-        completion(timeline)
+        Task {
+            let entry = try await SignEntry(date: .now, sign: getSign())
+            let timeline = Timeline(entries: [entry], policy: .after(.now.advanced(by: 5 * 60)))
+            completion(timeline)
+        }
+    }
+
+    private func getSign() async throws -> SignModel {
+        let groupHeaderFields = SignProvider.widgetGroup?.data(forKey: "groupHTTPHeaderFieldsKey")
+        var headers = try? JSONDecoder().decode([String : String].self, from: groupHeaderFields ?? Data())
+        headers?["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"
+        // swiftlint:disable force_unwrapping
+        let url = URL(string: "https://www.chongbuluo.com/plugin.php?id=wq_sign")!
+        // swiftlint:enble force_unwrapping
+        var requset = URLRequest(url: url)
+        requset.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: requset)
+        var signModel = SignModel()
+        if let html = try? HTML(html: data, encoding: .utf8) {
+            let sign = html.at_xpath("//a[@class='wqpc_sign_btn_red']")
+            let sign_btn = html.at_xpath("//div[@class='wqpc_sign_btna']")
+            var text_btn = signModel.signText
+            if let text = sign?.text {
+                text_btn = text
+            } else if let text = sign_btn?.text {
+                text_btn = text
+            }
+            signModel.signText = text_btn.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        return signModel
     }
 }
 
 struct SignEntry: TimelineEntry {
     var date: Date
+    var sign: SignModel
+}
+
+struct SignModel: Identifiable {
+    var id = UUID()
+    var signText = "今日未签到，点击签到"
 }
 
 @available (iOSApplicationExtension 16, *)
@@ -40,7 +76,8 @@ struct SignWidgetEntryView: View {
     var body: some View {
         switch family {
         case .accessoryInline:
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(entry.sign.signText)
+                .widgetURL(URL(string: "msea://daysign"))
         case .accessoryRectangular:
             HStack(alignment: .center, spacing: 0) {
                 VStack(alignment: .leading) {
@@ -53,7 +90,7 @@ struct SignWidgetEntryView: View {
             }
         case .accessoryCircular:
             ZStack {
-                AccessoryWidgetBackground()
+//                AccessoryWidgetBackground()
 
                 Image(systemName: "leaf.fill")
                     .resizable()
@@ -85,11 +122,11 @@ struct SignWidget: Widget {
 struct SignWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            SignWidgetEntryView(entry: SignEntry(date: .now))
+            SignWidgetEntryView(entry: SignEntry(date: .now, sign: SignModel()))
                 .previewContext(WidgetPreviewContext(family: .accessoryInline))
-            SignWidgetEntryView(entry: SignEntry(date: .now))
+            SignWidgetEntryView(entry: SignEntry(date: .now, sign: SignModel()))
                 .previewContext(WidgetPreviewContext(family: .accessoryRectangular))
-            SignWidgetEntryView(entry: SignEntry(date: .now))
+            SignWidgetEntryView(entry: SignEntry(date: .now, sign: SignModel()))
                 .previewContext(WidgetPreviewContext(family: .accessoryCircular))
         }
     }
